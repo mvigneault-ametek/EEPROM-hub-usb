a7234878ab3d3f711d815bf31a0dd29f04b1f448
mvigneault-ametek <55108922+mvigneault-ametek@users.noreply.github.com>
2019-09-16 11:14:28 -0400

    Create EEPROM-hub-usb.ino

---
+++ EEPROM-hub-usb.ino
@@ -0,0 +1,8 @@
+// This is an arduino program to set the right register in the I²C eeprom
+ void setup(){
+    // Here goes the setup loop
+ }
+ 
+ void loop(){
+    // Here goes the infinite loop
+ }
